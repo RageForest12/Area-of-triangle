a866e2889445820201c84c44fe6d8e11b91b4bdc
Maaz Ahmed <89376942+RageForest12@users.noreply.github.com>
2022-09-10 13:52:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#area of triangle 
+
+base = input('Enter Base ' )
+base = int(base)
+
+height = input('Enter height')
+height = int(height)
+
+# Calculatating the value
+
+answer1 = height * base
+answer2 = answer1 / 2
+
+answer2 = str(answer2)
+
+print('Your Answer is ' + answer2)
+
+
+#sub to codewithmaaz
